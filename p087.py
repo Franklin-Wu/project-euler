58502e0d4616f93dd1e4c4befba4bd8e714b143e
Franklin Wu <frankie.wu@gmail.com>
2017-01-30 02:04:10 -0800

    Better algorithm improves execution time from several hours (projected) to 1.4 seconds on Macbook Air Mid 2009, 2.13GHz Intel Core 2 Duo.

--- p087.py
+++ p087.py
@@ -28,24 +28,14 @@ infile.close();
 def print_execution_time():
     print "Execution time = %f seconds." % (time.time() - start_time);
 
-# Optimized over "n in primes" by exiting once the prime tested is greater than n.
-def is_prime(n):
-    n_is_prime = False;
-    for prime in primes:
-        if prime == n:
-            n_is_prime = True;
-            break;
-        if prime > n:
-            break;
-    return n_is_prime;
-
-# Returns the square root if n is a perfect square and the squre root is prime. Returns 0 otherwise.
-def get_prime_square_root(n):
-    prime_square_root = 0;
-    square_root = int(math.sqrt(n));
-    if ((square_root ** 2) == n) and is_prime(square_root):
-        prime_square_root = square_root;
-    return prime_square_root;
+squares = [];
+i = 0;
+while True:
+    square = primes[i] ** 2;
+    if square > MAXIMUM:
+        break;
+    squares.append(square);
+    i += 1;
 
 cubes = [];
 i = 0;
@@ -66,43 +56,23 @@ while True:
     i += 1;
 
 '''
+print squares;
+print len(squares);
 print cubes;
 print len(cubes);
 print tesseracts;
 print len(tesseracts);
 '''
 
-solution_count = 0;
-for n in range(MAXIMUM):
-    n_determined = False;
-    for t in tesseracts:
-        n_minus_t = n - t;
-        if n_minus_t <= 0:
-            break;
-        for c in cubes:
-            n_minus_t_minus_c = n_minus_t - c;
-            if n_minus_t_minus_c <= 0:
-                break;
-            if get_prime_square_root(n_minus_t_minus_c):
-                n_determined = True;
-                solution_count += 1;
-                # Print every 1000 solutions as a progress indicator.
-                if solution_count % 1000 == 0:
-                    print n;
-                '''
-                square_root = get_prime_square_root(n_minus_t_minus_c);
-                cube_root = primes[cubes.index(c)];
-                tesseract_root = primes[tesseracts.index(t)];
-                print "%d = %d^2 + %d^3 + %d^4" % (n, square_root, cube_root, tesseract_root);
-                '''
-                break;
-        if n_determined:
-            break;
-print;
-print "solution_count = %d." % (solution_count);
+solutions = set();
+for tesseract in tesseracts:
+    for cube in cubes:
+        for square in squares:
+            solution = square + cube + tesseract;
+            if solution <= MAXIMUM:
+                # print "%d = %d^2 + %d^3 + %d^4" % (solution, primes[squares.index(square)], primes[cubes.index(cube)], primes[tesseracts.index(tesseract)]);
+                solutions.add(solution);
+# print solutions;
+print "solution_count = %d." % len(solutions);
 
 print_execution_time();
-
-'''
-This would probably work, but is very slow--projecting to several hours on a Macbook Air Mid 2009, 2.13GHz Intel Core 2 Duo.
-'''
